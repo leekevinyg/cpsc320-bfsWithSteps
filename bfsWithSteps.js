07eb7ccd7aeecf37f827bdd150650ee9200ba852
Kevin <klee2010@gmail.com>
2019-07-14 12:09:25 -0700

    Update bfsWithSteps.js

--- bfsWithSteps.js
+++ bfsWithSteps.js
@@ -1,31 +1,29 @@
 const G = {
     'A': {
-        rating: "M",
-        costToUpgrade: 0,
-        edges: ['B', 'C', 'D'],
+        name: 'A',
+        edges: ['B', 'C'],
         visited: false,
         steps: 0,
     },
     'B': {
-        rating: "M",
-        costToUpgrade: 0,
-        edges: ['A', 'C', 'D'],
+        name: 'B',
+        edges: ['A', 'C'],
         visited: false,
         steps: 0,
     },
     'C': {
-        rating: "M",
-        costToUpgrade: 0,
-        edges: ['B', 'A', 'D'],
+        name: 'C',
+        edges: ['A', 'B'],
         visited: false,
         steps: 0,
     },
-    'D': {
-        rating: "M",
-        costToUpgrade: 0,
-        edges: ['A', 'B', 'C'],
-        visited: false,
-        steps: 0,
+    edges: {
+        'AB' : 'M',
+        'BA' : 'M',
+        'AC' : 'M',
+        'CA' : 'M',
+        'CB' : 'R',
+        'BC' : 'R',  
     }
 }
 
@@ -38,13 +36,23 @@ const BFSWithSteps = (G) => {
 
     while (Q.length > 0) {
         let vertex = Q.shift();
+        v = vertex.name;
         if (!vertex.visited) {
+            console.log(`visiting vertex ${vertex.name}`);
             vertex.visited = true;
+            console.log(`vertex is ${vertex.name} has neighbors ${vertex.edges}`);
             vertex.edges.forEach((neighbor) => {
-                if (G[neighbor].rating === 'R') {
+                if (v === neighbor) return;
+                let ratingOfEdge = G.edges[`${v}${neighbor}`];
+                let edge = `${v}${neighbor}`;
+                console.log(`edge ${edge} has rating ${ratingOfEdge}`);
+                if (ratingOfEdge === 'R') {
+                    console.log(`NOT incrementing steps to get to ${G[neighbor].name}`);
                     Q.push(G[neighbor]);
                 } else {
-                    G[neighbor].steps = G[neighbor].steps + 1;
+                    let newNumSteps = G[neighbor].steps + 1
+                    console.log(`incrementing steps to get to ${G[neighbor].name} to ${newNumSteps}`);
+                    G[neighbor].steps = newNumSteps;
                     Q.push(G[neighbor]);
                 }
             });
